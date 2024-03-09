e79834d0fdd610dd10161addb2197736972fae28
prinzeval <valentineallpowers@gmail.com>
2024-03-09 19:23:36 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template,jsonify
+
+
+app = Flask(__name__)
+JOBS = [
+    {
+        'id':1,
+        'title': 'Data Analyst',
+        'location': 'Newyork NY, USA',
+        'salary':  '$2,000'
+    },
+      {
+        'id':2,
+        'title': 'Data Scienstist',
+        'location': 'California, Usa',
+        
+
+    },
+      {
+        'id':3,
+        'title': 'Data Engineer',
+        'location': 'Remote',
+        'salary':  '$150,000'
+    },
+      {
+        'id':4,
+        'title': 'Data Entry Clerk',
+        'location': 'Ontario, Canada',
+        'salary':  '$5,000'
+    },
+]
+@app.route('/')
+def helen():
+    return render_template('home.html',
+                           jobs = JOBS,
+                           company_name = 'Meta')
+@app.route("/jobs")
+
+
+def list_jobs ():
+    return jsonify(JOBS)
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', debug=True)
+
+
+
+
