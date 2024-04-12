9c554b0f065884c1abcbaa85e956881690f52bfb
prinzeval <valentineallpowers@gmail.com>
2024-04-12 19:32:24 +0300

    Changes in database.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template,jsonify
-from database import engine,load_jobs_from_db
+from database import engine,load_jobs_from_db,load_job_from_db
 from sqlalchemy import text
 
 
@@ -19,6 +19,17 @@ def list_jobs ():
     my_jobs = load_jobs_from_db()
     return jsonify(my_jobs)
 
-if __name__ == '__main__':
+@app.route("/job/<id>") 
+def show_job(id): 
+    job = load_job_from_db(id)
+    if job is None:
+        return "Job not found."  # Display a message when job is not found
+    else:
+        return render_template('jobpage.html', job=job)
+
+
+
+if __name__ == '__main__': 
     app.run(host = '0.0.0.0', debug=True)
+
 
--- database.py
+++ database.py
@@ -2,16 +2,6 @@ from sqlalchemy import create_engine, text
 
 engine = create_engine("mysql+pymysql://root:Vondabaic2020@localhost/Val_Data")
 
-with engine.connect() as conn:
-    dict_list = []
-    result = conn.execute(text("SELECT * FROM jobs"))  
-    All_result = result.fetchall()
-    colunm_result = result.keys()
-    dict_result =[dict(zip(colunm_result,row))for row in All_result]
-    dict_list.append(dict_result)
-
-
-
 def load_jobs_from_db():
     with engine.connect() as conn:
         result = conn.execute(text("SELECT * FROM jobs"))
@@ -20,3 +10,16 @@ def load_jobs_from_db():
         colunm_result = result.keys()
         dict_result =[dict(zip(colunm_result,row))for row in All_result]
         return dict_result   
+    
+    
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(
+            text("SELECT * FROM jobs WHERE id = :val").bindparams(val=id)
+        )
+        rows = result.fetchall()
+        if len(rows) == 0:
+            return None
+        else:
+            # Construct a list of dictionaries for each row
+            return [{key: value for key, value in zip(result.keys(), row)} for row in rows]
