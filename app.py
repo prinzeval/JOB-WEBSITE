aa09a09661a8b548aaae6f76c23d3512505a614e
prinzeval <valentineallpowers@gmail.com>
2024-06-15 12:33:04 +0300

    Added a commented flask web app

--- app.py
+++ app.py
@@ -34,3 +34,79 @@ if __name__ == '__main__':
     app.run(host = '0.0.0.0', debug=True)
 
 
+
+
+
+
+
+# from flask import Flask,jsonify,request
+# from flask_sqlalchemy import SQLAlchemy
+# # from database import 
+
+# # Initialize Flask application
+# app = Flask(__name__)
+# # Configure the SQLite database URI
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+
+# # Initialize SQLAlchemy extension
+# db = SQLAlchemy(app)
+
+# # Define your model
+# class Drink(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(80), unique=True, nullable=False)
+#     description = db.Column(db.String(100))
+
+#     def __repr__(self):
+#         return f"{self.name} - {self.description}"
+
+# # Create the database tables within the application context
+# with app.app_context():
+#     db.create_all()
+
+# # Define your routes
+# @app.route('/')
+# def index():
+#     return "hello!"
+
+# @app.route('/drinks')
+# def get_drinks():
+#     ALL = Drink.query.all()
+#     output = []
+#     for drinks in ALL:
+#         drink_data = {"name":drinks.name,"description":drinks.description}
+#         output.append(drink_data)
+#     return {"drink": output}
+
+# @app.route('/drinks/<id>')
+# def get_drink(id):  
+#     DD = Drink.query.get(id)
+#     if not DD:
+#         return "NOT FOUND", 404
+    
+#     return jsonify({"name":DD.name,"description":DD.description})
+
+# @app.route('/drinks', methods=['POST'])
+# def add_drink():
+   
+#     drink = Drink(name=request.json['name'], description=request.json['description'])
+#     db.session.add(drink)
+#     db.session.commit()
+#     return {'id': drink.id}
+# @app.route('/drinks/<id>', methods = ['DELETE'])
+# def delete_drink(id):
+#     drink = Drink.query.get(id)
+#     if drink:
+#         db.session.delete(drink)
+#         db.session.commit()
+#         return{"message":"yeet!@"}
+#     else:
+#         return "404 NOT_FOUND"
+
+# # Run the Flask application
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+
+
