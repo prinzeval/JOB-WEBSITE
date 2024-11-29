048ce3688f718ce0010074a1145c1697e8e541ce
prinzeval <valentineallpowers@gmail.com>
2024-11-29 23:42:37 +0200

    Sxrapping  done moving it  a new folder as fastapi

--- models.py
+++ models.py
@@ -6,12 +6,12 @@ Base = declarative_base()
 class JobListing(Base):
     __tablename__ = 'Job_listing'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    position = Column(String, nullable=False)  # VARCHAR
-    company_name = Column(String, nullable=False)  # VARCHAR
-    location = Column(String, nullable=False)  # VARCHAR
-    salary = Column(String, nullable=True)  # VARCHAR
-    job_link = Column(String, nullable=False)  # VARCHAR
-    benefits = Column(String, nullable=True)  # VARCHAR
-    description = Column(String, nullable=True)  # VARCHAR
-    employment_type = Column(String, nullable=True)  # VARCHAR
+    "id" = Column(Integer, primary_key=True, autoincrement=True)
+    "POSITION" = Column(String, nullable=False)  # VARCHAR
+    "COMPANY NAME" = Column(String, nullable=False)  # VARCHAR
+    "LOCATION" = Column(String, nullable=False)  # VARCHAR
+    "SALARY" = Column(String, nullable=True)  # VARCHAR
+    "JOB LINK" = Column(String, nullable=False)  # VARCHAR
+    "BENEFITS" = Column(String, nullable=True)  # VARCHAR
+    "DESCRIPTION" = Column(String, nullable=True)  # VARCHAR
+    "EMPLOYMENT TYPE" = Column(String, nullable=True)  # VARCHAR
